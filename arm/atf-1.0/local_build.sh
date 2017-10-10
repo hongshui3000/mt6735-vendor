9ef80b6b41ffb8621e6868ca4dfc7a8ff9cc9e92
peishengguo <peishengguo@yydrobo.com>
2017-10-10 13:22:14 +0800

    提交mt6735

---
+++ arm/atf-1.0/local_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CFLAGS='-O0 -gdwarf-2' \
+CROSS_COMPILE=../../../prebuilts/gcc/linux-x86/aarch64/linaro-aarch64-linux-gnu-4.8/bin/aarch64-linux-gnu- \
+
+#make DEBUG=1 PLAT=mt6752 all
+make DEBUG=1 PLAT=mt6735 all
+#make DEBUG=1 PLAT=fvp all
+
+#make DEBUG=1 PLAT=mt6752 SPD=tspd all
+
+
